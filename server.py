f8217664d21dce9d0dd4f6b0117d7babad9df144
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:39:53 +0530

    updated the server code for client status

--- server.py
+++ server.py
@@ -2,34 +2,53 @@ import socket
 import threading
 import sys
 
-clients = []
-running = True  
+clients = {}
+client_names = {}
+
+def broadcast_clients():
+    """Send the list of connected clients to all clients."""
+    client_list = ','.join(client_names.values())
+    for client in clients.values():
+        try:
+            client.send(f"CLIENT_LIST:{client_list}".encode('utf-8'))
+        except:
+            pass
+
+def handle_client(client, addr):
+    """Handle communication with each connected client."""
+    client_name = f"Client {addr[1]}"
+    client_names[client] = client_name
+    clients[client_name] = client
+    print(f"{client_name} connected")
+    
+    broadcast_clients()  
+
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message.startswith("PRIVATE:"):
+                target_client_name = message.split(":")[1].split(" ")[0]
+                private_message = " ".join(message.split(":")[1].split(" ")[1:])
+                if target_client_name in clients:
+                    clients[target_client_name].send(f"PRIVATE {client_name}: {private_message}".encode('utf-8'))
+            elif message:
+                broadcast(message, client, client_name)
+        except:
+            break
+
+    del clients[client_name]
+    del client_names[client]
+    client.close()
+    broadcast_clients()
 
 def broadcast(message, client, client_name):
     """Send the message to all clients except the sender."""
-    for c in clients:
+    for c in clients.values():
         if c != client:
             try:
                 c.send(f"{client_name}: {message}".encode('utf-8'))
             except:
                 clients.remove(c)
-
-def handle_client(client, addr):
-    """Handle communication with each connected client."""
-    client_name = f"Client {addr[1]}"  
-    print(f"{client_name} connected")
-    
-    while running:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            if message:
-                broadcast(message, client, client_name)
-            else:
-                break
-        except:
-            break
-    clients.remove(client)
-    client.close()
 
 def start_server():
     """Start the server and accept incoming client connections."""
@@ -40,15 +59,13 @@ def start_server():
 
     global running
     try:
-        while running:
+        while True:
             client, addr = server.accept()
-            clients.append(client)
             threading.Thread(target=handle_client, args=(client, addr), daemon=True).start()
     except KeyboardInterrupt:
         print("\nServer is shutting down...")
-        running = False  
     finally:
-        for client in clients:
+        for client in clients.values():
             client.close()
         server.close()
         sys.exit(0)  
