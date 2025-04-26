2eebc54814cb6cd1720b4bdcfb53fdf2c20003ff
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:12:17 +0530

    handling single clients

--- server.py
+++ server.py
@@ -10,3 +10,16 @@ def broadcast(message, client):
                 c.send(message)
             except:
                 clients.remove(c)
+
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            if message:
+                broadcast(message, client)
+            else:
+                break
+        except:
+            break
+    clients.remove(client)
+    client.close()
