7a3fa8dc3f397c1b682986a4dd20a47a14b52524
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:13:36 +0530

    function for retrieval of messages

--- client.py
+++ client.py
@@ -1,3 +1,10 @@
 import socket 
 import threading
 
+def receive_messages(client):
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            break
