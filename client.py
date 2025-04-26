0283e69d569756d1de1f3be6f7c039e7b0fb5347
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:13:56 +0530

    startup of client

--- client.py
+++ client.py
@@ -8,3 +8,15 @@ def receive_messages(client):
             print(message)
         except:
             break
+
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 5555))
+
+    threading.Thread(target=receive_messages, args=(client,)).start()
+
+    while True:
+        message = input()
+        client.send(message.encode('utf-8'))
+
+start_client()
