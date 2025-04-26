c7de28bee46321ebc79811193b2154b43d35d40a
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:18:30 +0530

    integrated gui in the client itself

--- client.py
+++ client.py
@@ -1,22 +1,46 @@
 import socket 
-import threading
+import threading 
+import tkinter as tk
+from tkinter import scrolledtext
 
-def receive_messages(client):
+def receive_messages(client, text_area):
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            print(message)
+            text_area.insert(tk.END, message + '\n')
+            text_area.yview(tk.END)  
         except:
             break
+
+def send_message(client, message_entry, text_area):
+    message = message_entry.get()
+    if message:
+        client.send(message.encode('utf-8'))
+        text_area.insert(tk.END, "You: " + message + '\n') 
+        text_area.yview(tk.END)  
+        message_entry.delete(0, tk.END)
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('127.0.0.1', 5555))
 
-    threading.Thread(target=receive_messages, args=(client,)).start()
+    root = tk.Tk()
+    root.title("Chat Client")
 
-    while True:
-        message = input()
-        client.send(message.encode('utf-8'))
+    text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=20)
+    text_area.pack(padx=10, pady=10)
+    text_area.config(state=tk.DISABLED)  
 
-start_client()
+    message_entry = tk.Entry(root, width=40)
+    message_entry.pack(padx=10, pady=10)
+
+    message_entry.bind("<Return>", lambda event: send_message(client, message_entry, text_area))
+
+    send_button = tk.Button(root, text="Send", command=lambda: send_message(client, message_entry, text_area))
+    send_button.pack(pady=10)
+
+    threading.Thread(target=receive_messages, args=(client, text_area), daemon=True).start()
+
+    root.mainloop()
+
+start_client()  
