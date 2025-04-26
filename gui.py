95bded25cf9b6d8a8e09a9ff704d2ba99cc81160
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:15:20 +0530

    ui for receiving messages

--- gui.py
+++ gui.py
@@ -3,3 +3,11 @@ import threading
 import tkinter as tk
 from tkinter import scrolledtext
 
+def receive_messages(client, text_area):
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            text_area.insert(tk.END, message + '\n')
+            text_area.yview(tk.END)  # Auto-scroll to the latest message
+        except:
+            break
