3d650b6bbfb813b502af66a8d563a8321fbc36e8
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:24:11 +0530

    modified the code for keyboard interrupt

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import socket
 import threading
+import sys
 
 clients = []
 
 def broadcast(message, client):
+    """Send the message to all clients except the sender."""
     for c in clients:
         if c != client:
             try:
@@ -12,6 +14,7 @@ def broadcast(message, client):
                 clients.remove(c)
 
 def handle_client(client):
+    """Handle communication with each connected client."""
     while True:
         try:
             message = client.recv(1024)
@@ -25,15 +28,23 @@ def handle_client(client):
     client.close()
 
 def start_server():
+    """Start the server and accept incoming client connections."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('127.0.0.1', 5555))
     server.listen(5)
     print("Server is running and waiting for connections...")
 
-    while True:
-        client, addr = server.accept()
-        clients.append(client)
-        print(f"New connection: {addr}")
-        threading.Thread(target=handle_client, args=(client,)).start()
+    try:
+        while True:
+            client, addr = server.accept()
+            clients.append(client)
+            print(f"New connection: {addr}")
+            threading.Thread(target=handle_client, args=(client,)).start()
+    except KeyboardInterrupt:
+        print("\nServer is shutting down...")
+        for client in clients:
+            client.close()
+        server.close()
+        sys.exit(0)  
 
 start_server()
