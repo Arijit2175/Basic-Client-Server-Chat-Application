d433c5591754b6b70c7465298550aa56bdbb91f8
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:11:53 +0530

    broadcasting the message

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
 import socket
 import threading
 
+clients = []
+
+def broadcast(message, client):
+    for c in clients:
+        if c != client:
+            try:
+                c.send(message)
+            except:
+                clients.remove(c)
