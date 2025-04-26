9eb6f5c31c5853bff4b42396584e61820fb889ee
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-26 10:15:44 +0530

    ui for sending messages

--- gui.py
+++ gui.py
@@ -8,6 +8,14 @@ def receive_messages(client, text_area):
         try:
             message = client.recv(1024).decode('utf-8')
             text_area.insert(tk.END, message + '\n')
-            text_area.yview(tk.END)  # Auto-scroll to the latest message
+            text_area.yview(tk.END)  
         except:
             break
+
+def send_message(client, message_entry, text_area):
+    message = message_entry.get()
+    if message:
+        client.send(message.encode('utf-8'))
+        text_area.insert(tk.END, "You: " + message + '\n') 
+        text_area.yview(tk.END)  
+        message_entry.delete(0, tk.END)  
